2cbbafffd94fe7a870498ba4408184718126fa78
Juan Diego Melo <jdmelo002@gmail.com>
2020-07-24 18:31:37 -0500

    Comando img

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+import requests
+
+from bs4 import BeautifulSoup
 
 class BasicCommands(commands.Cog):
 
@@ -20,3 +23,15 @@ class BasicCommands(commands.Cog):
         embed.add_field(name="Región del servidor", value=f"{ctx.guild.region}")
         embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def img(self, ctx, *, search):
+        url = f"https://www.google.co.in/search?q={search}&source=lnms&tbm=isch"
+
+        page = requests.get(url)
+        if page.status_code == 200:
+            embed = discord.Embed(title=search, color=discord.Color.dark_magenta())
+            page = BeautifulSoup(page.text, "lxml")
+            img = page.find("img", attrs={"class": "t0fcAb"})
+            embed.set_image(url=f"{img.get('src')}")
+            await ctx.send(embed=embed)
