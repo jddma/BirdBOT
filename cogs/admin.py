7df01d8463dfae4ced820c028364b51c77d44cf8
Juan Diego Melo <jdmelo002@gmail.com>
2020-08-02 16:37:13 -0500

    Primer comando de administración

---
+++ cogs/admin.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.__bot = bot
+
+    #Comando que sirve para asignar un rol a un usuario, para ejecutarlo el miembro debe tener el permiso de administración de roles
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def setrole(self, ctx, member_mention, role_mention):
+        member = discord.utils.get(ctx.guild.members, mention=member_mention)
+        role = discord.utils.get(ctx.guild.roles, mention=role_mention)
+
+        #Se asegura que el miembro y el rol enviados existan en el servidor
+        if (member is None) or (role is None):
+            embed = discord.Embed(title="Error", description="Argumentos incorrectos", color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+        else:
+            #En caso de que el bot no tenga los permisos para realizar la acción
+            try:
+                await member.add_roles(role)
+                embed = discord.Embed(title="Rol asignado", description=f"Al miembro {member_mention} se le ha asignado el rol {role_mention}", color=discord.Color.green())
+                await ctx.send(embed=embed)
+
+            except discord.errors.Forbidden:
+                embed = discord.Embed(title="Error", description="No tengo permisos suficientes para esto", color=discord.Color.red())
+                await ctx.send(embed=embed)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from util.settings import Vars
 from cogs.basic import BasicCommands
 from cogs.events import Events
 from cogs.tournament import Tournament
+from cogs.admin import Admin
 
 class BirdBOT(commands.Bot):
 
@@ -16,6 +17,7 @@ class BirdBOT(commands.Bot):
         self.add_cog(BasicCommands(self, self.vars.get_sounds()))
         self.add_cog(Events(self))
         self.add_cog(Tournament(self))
+        self.add_cog(Admin(self))
 
 
 if __name__ == '__main__':
