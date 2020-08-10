761f7e33a1ed073daf4fa7c05fc9dea65ebb2c8d
Juan Diego Melo <jdmelo002@gmail.com>
2020-08-10 16:30:30 -0500

    Comando para retirar un rol

--- cogs/admin.py
+++ cogs/admin.py
@@ -30,6 +30,27 @@ class Admin(commands.Cog):
                 embed = discord.Embed(title="Error", description="No tengo permisos suficientes para esto", color=discord.Color.red())
                 await ctx.send(embed=embed)
 
-    @setrole.error
-    async def setrole_error(self, ctx, error):
-        pass
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def unsetrole(self, ctx, member_mention, role_mention):
+        member = discord.utils.get(ctx.guild.members, mention=member_mention)
+        role = discord.utils.get(ctx.guild.roles, mention=role_mention)
+
+        # Se asegura que el miembro y el rol enviados existan en el servidor
+        if (member is None) or (role is None):
+            embed = discord.Embed(title="Error", description="Argumentos incorrectos", color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+        else:
+            # En caso de que el bot no tenga los permisos para realizar la acción
+            try:
+                await member.remove_roles(role)
+                embed = discord.Embed(title="Rol asignado",
+                                      description=f"Al miembro {member_mention} se le ha intentado retirado el rol {role_mention}",
+                                      color=discord.Color.green())
+                await ctx.send(embed=embed)
+
+            except discord.errors.Forbidden:
+                embed = discord.Embed(title="Error", description="No tengo permisos suficientes para esto",
+                                      color=discord.Color.red())
+                await ctx.send(embed=embed)
